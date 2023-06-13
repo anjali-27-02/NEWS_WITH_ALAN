e48c0786fa414dbe0020a0bedeedb1dd662a8f13
Anjali <erbhardwajanjali27@gmail.com>
2023-06-13 15:07:03 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+import NewsCards from "./components/NewsCards/NewsCards";
+import useStyles from './styles';
+// import { Typography } from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const alanKey='a60da7b8ce587373c6c46e2afedff8052e956eca572e1d8b807a3e2338fdd0dc/stage';
+const App=()=>{
+    const [newsArticle, setnewsArticle] = useState([]);
+    const classes=useStyles();
+    useEffect(() => {
+        alanBtn({
+            key:alanKey,
+            onCommand:({commandData,articles})=>{
+                if(commandData==="News"){
+                    // console.log(articles.articles)
+                    setnewsArticle(articles.articles)
+                }
+                // else if(commandData==='open'){
+                //     window.open(articles[number].url,'_blank')
+                // }
+            }
+        })
+    }, [])
+    
+    return(
+        <div>
+        <div className={classes.logoContainer}>
+          <img src="https://media.gettyimages.com/id/1273758425/video/global-earth-rotating-digital-world-news-studio-stock-video.jpg?s=640x640&k=20&c=2JGgtJYAE6bncYxKl2fSVXfGoF7L95uXDC5F15tipFs=" className={classes.alanLogo} alt="logo" />
+        </div>
+        <NewsCards article={newsArticle}/>
+        </div>
+    )
 }
-
 export default App;
